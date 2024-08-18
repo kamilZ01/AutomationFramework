eceb505e685c329ff19a78c68462064d2926d963
kamilZ01 <zarychkamil@gmail.com>
2024-08-18 16:18:04 +0200

    Add `TagCheckerUtil` class and update `AppHooks` class to utilize `TagCheckerUtil`

--- src/test/java/hooks/AppHooks.java
+++ src/test/java/hooks/AppHooks.java
@@ -10,12 +10,15 @@ import org.automation.framework.utils.ConfigReader;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import utils.TagCheckerUtil;
 
 public class AppHooks {
 
     private Scenario scenario;
     private WebDriver driver;
     private final ScenarioContext scenarioContext;
+    private final TagCheckerUtil tagCheckerUtil = new TagCheckerUtil();
+    private boolean isRestScenario;
 
     public AppHooks(ScenarioContext scenarioContext) {
         this.scenarioContext = scenarioContext;
@@ -29,8 +32,8 @@ public class AppHooks {
     public void launchBrowser(Scenario scenario) {
         ConfigReader configReader = new ConfigReader();
         configReader.initProp();
-        boolean isRest = scenario.getSourceTagNames().contains("@Rest");
-        if (!isRest) {
+        isRestScenario = tagCheckerUtil.hasTestAPITag(scenario);
+        if (!isRestScenario) {
             BrowserType browser = configReader.getBrowser();
             DriverManager driverFactory = new DriverManager();
             driver = driverFactory.initDriver(browser);
@@ -40,12 +43,14 @@ public class AppHooks {
 
     @After(order = 0)
     public void quitBrowser() {
-        driver.quit();
+        if (!isRestScenario) {
+            driver.quit();
+        }
     }
 
     @After(order = 1)
     public void tearDown(Scenario scenario) {
-        if (scenario.isFailed()) {
+        if (scenario.isFailed() && !isRestScenario) {
             String screenshotName = scenario.getName().replaceAll(" ", "_");
             byte[] sourcePath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
             scenario.attach(sourcePath, "image/png", screenshotName);
